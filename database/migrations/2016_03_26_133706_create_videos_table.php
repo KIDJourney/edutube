84937f10c359205c8d2769e9d5cfd29ad01a93a8
KIDJourney <kingdeadfish@qq.com>
2016-03-26 22:00:28 +0800

    create videos , series schema

---
+++ database/migrations/2016_03_26_133706_create_videos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',25)->index();
+            $table->string('description',255);
+            $table->string('video_url',255);
+
+            $table->unsignedInteger('user_id')->index();
+            $table->unsignedInteger('series_id')->index();
+
+            $table->unsignedInteger('watch_count')->default(0)->index();
+            $table->unsignedInteger('reply_count')->default(0)->index();
+            $table->unsignedInteger('favorite_count')->default(0)->index();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('videos');
+    }
+}
---
+++ database/migrations/2016_03_26_133741_create_series_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->string('description')->nullable();
+            $table->unsignedInteger('count')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('series');
+    }
+}
